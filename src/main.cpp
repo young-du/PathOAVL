b5f30ef4241e06624b19450563dd9ffc2d69a541
duyung <duyung@umich.edu>
2020-07-01 16:05:29 -0400

    indent

--- src/main.cpp
+++ src/main.cpp
@@ -13,27 +13,27 @@ using namespace std;
 
 int main() {
     int bucketSize = 4;
-	int numBlocks = pow(2, 12);
-	Bucket::setMaxSize(bucketSize);	
+    int numBlocks = pow(2, 12);
+    Bucket::setMaxSize(bucketSize);    
 
-	UntrustedStorageInterface* storage = new ServerStorage();
-	RandForOramInterface* random = new RandomForOram();
-	OAVLTreeInterface* oavl = new OAVLTreePathEviction(storage, random, bucketSize, numBlocks);
+    UntrustedStorageInterface* storage = new ServerStorage();
+    RandForOramInterface* random = new RandomForOram();
+    OAVLTreeInterface* oavl = new OAVLTreePathEviction(storage, random, bucketSize, numBlocks);
 
-	cout << "Test begins..." << endl;
+    cout << "Test begins..." << endl;
 
     cout << "insert " << 9<< endl;
     oavl->insert(9,9);
     storage->dumpAllValidBlocks();
     
-	for (int i = 7; i >= 1; i-=2){
+    for (int i = 7; i >= 1; i-=2){
         cout << "insert " << i<< endl;
         oavl->insert(i,i);
         storage->dumpAllValidBlocks();
         cout << "insert " << i+1<< endl;
         oavl->insert(i+1,i+1);
         storage->dumpAllValidBlocks();
-	}
+    }
     int res = oavl->search(9);
     cout << res << endl;
 }
